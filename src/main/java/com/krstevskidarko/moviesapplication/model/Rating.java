22e95d9617d7d9c980a93698ab9ff17c3c177645
krstevskiDarko <darko_krstevski@icloud.com>
2024-03-04 19:38:13 +0100

    Refactoring

--- src/main/java/com/krstevskidarko/moviesapplication/model/Rating.java
+++ src/main/java/com/krstevskidarko/moviesapplication/model/Rating.java
@@ -1,4 +1,4 @@
-package com.krstevskidarko.moviesapplication.model.model;
+package com.krstevskidarko.moviesapplication.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
